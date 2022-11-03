3a66b23ace00bcb99a0bd820bc1b907567bbaa7d
Bjarki Gram <bjarki@pcgram.dk>
2022-11-03 15:47:34 +0100

    Added the very specific linux package informatino to the linux shell script as well

--- tf-modules/lambda/scripts/create_pkg.sh
+++ tf-modules/lambda/scripts/create_pkg.sh
@@ -2,5 +2,5 @@ export lambda_func=$1
 export dir_name=${lambda_func}_lambda_dist_pkg
 export req=${lambda_func}/requirements.txt
 mkdir ${dir_name}
-pip3 install -r $(pwd)/${req} --target $(pwd)/${dir_name} --upgrade
+pip3 install -r $(pwd)/${req} --target $(pwd)/${dir_name} --platform manylinux2014_x86_64 --implementation cp --python 3.9 --only-binary=:all: --upgrade
 cp -R $(pwd)/${lambda_func}/. $(pwd)/${dir_name}

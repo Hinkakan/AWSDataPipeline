eab1ef9d83da296381fe8b09c0e1e5cc7ea4a95c
Bjarki Gram <bjarki@pcgram.dk>
2022-11-03 14:59:56 +0100

    Add secrets as envionment variables

--- tf-modules/lambda/scripts/create_pkg.sh
+++ tf-modules/lambda/scripts/create_pkg.sh
@@ -1,9 +1,6 @@
 export lambda_func=$1
 export dir_name=${lambda_func}_lambda_dist_pkg
-echo ${dir_name}
 export req=${lambda_func}/requirements.txt
-echo ${req}
 mkdir ${dir_name}
-ls
 pip3 install -r $(pwd)/${req} --target $(pwd)/${dir_name} --upgrade
 cp -R $(pwd)/${lambda_func}/. $(pwd)/${dir_name}

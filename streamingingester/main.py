5f8f986815b4dc63246d592995d9eb6b96d0b5fa
Bjarki Gram <bjarki@pcgram.dk>
2022-11-06 21:41:19 +0100

    push for commit

--- streamingingester/main.py
+++ streamingingester/main.py
@@ -55,7 +55,7 @@ def handler(event, context):
 
     logger.info(f"Received message: {event}")
     # Parse out data from message
-    payload = event["Records"][0]["body"][0]
+    payload = event["Records"][0]["body"]
     print(payload)
     # Create dict from payload string
     payload = json.loads(payload)

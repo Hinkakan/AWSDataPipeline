88ddf962db342087522d5e74fb1b197a2e901eaf
Bjarki Gram <bjarki@pcgram.dk>
2022-12-23 10:13:50 +0100

    Added test branch

--- tf-modules/lambda/scripts/create_pkg.sh
+++ tf-modules/lambda/scripts/create_pkg.sh
@@ -1,5 +1,6 @@
 export lambda_func=$1
-export dir_name=${lambda_func}_lambda_dist_pkg
+export env=$2
+export dir_name=${lambda_func}_${env}_lambda_dist_pkg
 export req=${lambda_func}/requirements.txt
 mkdir ${dir_name}
 pip3 install -r $(pwd)/${req} --target $(pwd)/${dir_name} --platform manylinux2014_x86_64 --implementation cp --python 3.9 --only-binary=:all: --upgrade
